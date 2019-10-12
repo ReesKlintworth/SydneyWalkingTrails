c1553a1f1fbb51cd4de957e5cf852e8bf6277fc5
Rees Klintworth <rees.klintworth@gmail.com>
2019-10-13 10:09:46 +1100

    Create new SwiftUI project with CoreData setup.

---
+++ SydneyWalkingTrails/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SydneyWalkingTrails
+//
+//  Created by Rees Klintworth on 10/13/19.
+//  Copyright © 2019 Rees Klintworth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
